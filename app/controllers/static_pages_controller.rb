d5b6b45ca15bbdf5ae7bcff7be3dd368ba41fc12
maynor romero <maynor55555@gmail.com>
2024-11-07 15:59:23 -0600

    guia 9

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+    #logica
+  end
+
+  def help
+    #logica
+  end
+
+  def about
+    #logica
+  end
+
+  def contact
+  end
+  
+end
